7c515d5fa999d3ae9d7610224d38edefc9fdbe13
jewellerybalaraam <jewellerybalaraam@gmail.com>
2026-05-16 20:14:44 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,17 +16,20 @@ app.get("/", (req, res) => {
   res.send("Jewel Tracker API Running");
 });
 
+console.log("MONGO URI EXISTS:", !!process.env.MONGO_URI);
+console.log("PORT:", process.env.PORT);
+
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
 
-    const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   })
   .catch((err) => {
-    console.log(err);
+    console.error("Mongo Error:", err);
   });
