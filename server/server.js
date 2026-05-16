a78e0cbdfc2de13a8e4efaee7a80878cdf5418bf
jewellerybalaraam <jewellerybalaraam@gmail.com>
2026-05-16 20:28:48 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ mongoose
   .then(() => {
     console.log("MongoDB Connected");
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
