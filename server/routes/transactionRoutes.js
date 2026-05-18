ff9137f33095165c99e2f2fc7398671a763c06b8
Saairam37 <saairam185@gmail.com>
2026-05-18 18:43:30 +0530

    small edit

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -1,26 +1,31 @@
-const express = require("express");
+const express = require(
+  "express"
+);
 
-const router = express.Router();
+const router =
+  express.Router();
 
 const {
   createTransaction,
-  getAllTransactions,
+  getTransactions,
   updateItemStatus,
-  searchBarcode,
-  customerHistory,
-  updatePCS,
-} = require("../controllers/transactionController");
+} = require(
+  "../controllers/transactionController"
+);
 
-router.post("/create", createTransaction);
+router.post(
+  "/",
+  createTransaction
+);
 
-router.get("/", getAllTransactions);
+router.get(
+  "/",
+  getTransactions
+);
 
-router.put("/update-status", updateItemStatus);
-
-router.get("/barcode/:barcode", searchBarcode);
-
-router.get("/customer/:customerName", customerHistory);
-
-router.put("/update-pcs", updatePCS);
+router.put(
+  "/:transactionId/:itemIndex",
+  updateItemStatus
+);
 
 module.exports = router;
