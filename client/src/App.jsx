1c900f1d60a4cd4e0ec6e73aced5f288ae6ed611
Saairam37 <saairam185@gmail.com>
2026-05-20 13:29:06 +0530

    appjsx fil

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,36 @@
-// client/src/App.jsx
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom'
 
 import NewTransaction from './pages/NewTransaction'
-
 import UploadInventory from './pages/UploadInventory'
+
+function Home() {
+
+  return (
+
+    <div style={{ padding: '20px' }}>
+
+      <h1>Jewellery ERP</h1>
+
+      <br />
+
+      <Link to="/upload">
+        Upload Inventory
+      </Link>
+
+      <br /><br />
+
+      <Link to="/transaction">
+        New Transaction
+      </Link>
+
+    </div>
+  )
+}
 
 function App() {
 
@@ -15,14 +41,19 @@ function App() {
       <Routes>
 
         <Route
-          path="/transaction"
-          element={<NewTransaction />}
+          path="/"
+          element={<Home />}
         />
 
         <Route
-  path="/upload"
-  element={<UploadInventory />}
-/>
+          path="/upload"
+          element={<UploadInventory />}
+        />
+
+        <Route
+          path="/transaction"
+          element={<NewTransaction />}
+        />
 
       </Routes>
 
